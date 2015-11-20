8bbec822e6eaf788eb60a8c3558603db730711f3
João Correia <lucaskm98@gmail.com>
2015-11-20 00:19:04 -0300

    Adicionado o servlet de logout de administrador

---
+++ src/controler/servletLogoutAdministrador.java
@@ -0,0 +1,45 @@
+package controler;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class servletLogoutAdministrador
+ */
+@WebServlet("/servletLogoutAdministrador.jsp")
+public class servletLogoutAdministrador extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servletLogoutAdministrador() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		session.invalidate();
+		
+		response.sendRedirect("index.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
